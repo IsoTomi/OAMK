cadd23fb471c77e2dcf8b41257530942cd783c2e
Tomi I <tomiisoj1984@gmail.com>
2020-01-23 22:35:55 +0200

    VK1T32

---
+++ Viikko 1/VK1T32/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("  CCCC\n"
+           " C    CC\n"
+           "C\n"
+           "C       - Ohjelmointi\n"
+           "C\n"
+           " C    CC\n"
+           "  CCCC\n\n");
+
+    return 0;
+}
